784eec4d3e521b07cd445f375ac77632db7f11e0
Jatin Kumar <jatin15011999@gmail.com>
2019-03-04 01:28:11 +0530

    exp

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // require('dotenv').config();
 
-const mongoURI = `mongodb+srv://user1999:${process.env.MONGO_ATLAS_PW}@food-tems-e14pe.mongodb.net/test?retryWrites=true`;
+const mongoURI = `mongodb+srv://user1999:mB82l2WJgiNUTzWz@food-tems-e14pe.mongodb.net/test?retryWrites=true`;
 
 mongoose.connect(mongoURI,{ useNewUrlParser: true }).then(()=>{
   console.log('Server connected to database!');
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
+// require('dotenv').config();
 const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {FoodItem} = require('./db/foodItems');
