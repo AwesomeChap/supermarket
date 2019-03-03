76a2fc6f1d55171c434bf1f9211ab9ff186983e3
Jatin Kumar <jatin15011999@gmail.com>
2019-03-04 00:50:45 +0530

    deployed

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,10 @@ import '.././../public/scss/header.scss';
 export default (props) => (
   <div className="header-section">
     <div className="heading">
-      <h1>Super<span>Market</span></h1>
+      <h1>Super<span>Market</span> <font color="#666">Food-info</font> </h1>
     </div>
     <div className="cart">
-      You have Selected <span>{props.items}</span> items
+      You have Selected <span>{props.items}</span> item{props.items > 1 ?'s' : ''}
     </div>
   </div>
 )
